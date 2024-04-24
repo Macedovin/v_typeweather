f3142880242f516b437146e8a6ea7c39b44a7e88
Vinicius Macedo <macedo.vp@gmail.com>
2024-04-24 18:06:22 -0300

    Finish 'getWeatherByCity.ts' and 'NextDaysItem'
    Convert and refactor component and API response files

--- src/services/getWeatherByCity.ts
+++ src/services/getWeatherByCity.ts
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 
 import { api } from "./api";
 import { getNextDays } from "../utils/getNextDays";
+import { NextDaysItemProps } from "../components/NextDaysItem";
 import { weatherIcons, WeatherIconsKeysProps } from "../utils/weatherIcons";
 
 export interface GetWeatherByCityProps {
@@ -11,6 +12,7 @@ export interface GetWeatherByCityProps {
 
 export interface WeatherAPIResponseProps {
   list: {
+    dt_txt: string;
     pop: number;
     main: {
       temp: number;
@@ -52,8 +54,8 @@ export async function getWeatherByCity({ latitude, longitude }: GetWeatherByCity
   }
 
   const days = getNextDays();
-  const daysAdded = [];
-  const nextDays = [];
+  const daysAdded: string[] = [];
+  const nextDays: NextDaysItemProps[] = [];
 
   data.list.forEach((item) => {
     const day = dayjs(new Date(item.dt_txt)).format('DD/MM');
